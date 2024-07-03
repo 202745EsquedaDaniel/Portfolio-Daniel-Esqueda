6c64213fe080fce4f582912f71f1c07d59b0bc9d
00lar <eduardani84@gmail.com>
2024-07-03 13:34:32 -0600

    porfolio v1

---
+++ src/components/Contact.tsx
@@ -0,0 +1,111 @@
+import emailjs from '@emailjs/browser';
+import React, { useRef, useState } from 'react';
+import { toast } from 'sonner';
+
+export const Contact = () => {
+	const form = useRef(null);
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [message, setMessage] = useState('');
+
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault();
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+		if (!name || !email || !message) {
+			toast.warning('Please fill in all fields.');
+			return;
+		}
+
+		if (!emailRegex.test(email)) {
+			toast.warning('Please enter a valid email address.');
+			return;
+		}
+		if (!form.current) return;
+		const promise = emailjs
+			.sendForm(import.meta.env.VITE_SERVICE_ID, import.meta.env.VITE_TEMPLATE_ID, form.current, {
+				publicKey: import.meta.env.VITE_PUBLIC_KEY,
+			})
+			.then(
+				() => {
+					setName('');
+					setEmail('');
+					setMessage('');
+				},
+				(error) => {
+					console.log('FAILED...', error.text);
+					toast.error('Failed to send email. Please try again.');
+				}
+			);
+		toast.promise(promise, {
+			loading: 'Loading...',
+			success: () => {
+				return 'Email sent successfully:';
+			},
+			error: 'Error sending email',
+		});
+	};
+
+	return (
+		<div className="text-lg w-full bg-[#1D1C20] rounded-lg p-6 lg:p-12 font-normal text-neutral-400">
+			<h2 className="text-2xl lg:text-4xl lg:pb-4 font-bold text-neutral-100">Contact</h2>
+			<div className="flex">
+				<form ref={form} className="my-8 w-full" onSubmit={handleSubmit}>
+					<div className="flex flex-col gap-4 mb-4">
+						<label htmlFor="name" className="text-xl text-neutral-300 font-semibold">
+							Name
+						</label>
+						<input
+							id="name"
+							name="user_name"
+							value={name}
+							onChange={(e) => setName(e.target.value)}
+							placeholder="Tyler Durden"
+							type="text"
+							className="bg-[#27272A] w-full outline-none focus:outline-cyan-500 px-4 py-2 rounded-lg border-none transition-all duration-300 ease-in-out text-neutral-300"
+						/>
+						<label htmlFor="email" className="text-xl text-neutral-300 font-semibold">
+							Email
+						</label>
+						<input
+							id="email"
+							placeholder="eduardani84@gmail.com"
+							type="text"
+							name="user_email"
+							value={email}
+							className="bg-[#27272A] w-full outline-none focus:outline-cyan-500 px-4 py-2 rounded-lg border-none transition-all duration-300 text-neutral-300 ease-in-out"
+							onChange={(e) => setEmail(e.target.value)}
+						/>
+						<label htmlFor="message" className="text-xl text-neutral-300 font-semibold">
+							Message
+						</label>
+						<textarea
+							id="message"
+							name="message"
+							value={message}
+							onChange={(e) => setMessage(e.target.value)}
+							placeholder="I want to collaborate with you to create a website for my social networks."
+							className="bg-[#27272A] w-full text-neutral-300 outline-none focus:outline-cyan-500 px-4 py-2 rounded-lg border-none transition-all duration-300 ease-in-out resize-none"
+						/>
+					</div>
+
+					<button
+						className="bg-gradient-to-br px-4 py-2 relative group/btn from-zinc-900 to-zinc-900 block bg-zinc-800 w-full text-white rounded-md font-medium shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]"
+						type="submit">
+						Send Email
+						<BottomGradient />
+					</button>
+				</form>
+			</div>
+		</div>
+	);
+};
+
+const BottomGradient = () => {
+	return (
+		<>
+			<span className="group-hover/btn:opacity-100 block transition duration-500 opacity-0 absolute h-px w-full -bottom-px inset-x-0 bg-gradient-to-r from-transparent via-cyan-500 to-transparent" />
+			<span className="group-hover/btn:opacity-100 blur-sm block transition duration-500 opacity-0 absolute h-px w-1/2 mx-auto -bottom-px inset-x-10 bg-gradient-to-r from-transparent via-indigo-500 to-transparent" />
+		</>
+	);
+};
---
+++ src/components/Icons.tsx
@@ -0,0 +1,380 @@
+export const InstagramIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M16.5 12C16.5 14.4853 14.4853 16.5 12 16.5C9.51472 16.5 7.5 14.4853 7.5 12C7.5 9.51472 9.51472 7.5 12 7.5C14.4853 7.5 16.5 9.51472 16.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M17.5078 6.5L17.4988 6.5"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export const Linkedin01Icon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M7 10V17"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M11 13V17M11 13C11 11.3431 12.3431 10 14 10C15.6569 10 17 11.3431 17 13V17M11 13V10"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M7.00801 7L6.99902 7"
+			stroke="currentColor"
+			strokeWidth="2"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export const Github01Icon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M6.51734 17.1132C6.91177 17.6905 8.10883 18.9228 9.74168 19.2333M9.86428 22C8.83582 21.8306 2 19.6057 2 12.0926C2 5.06329 8.0019 2 12.0008 2C15.9996 2 22 5.06329 22 12.0926C22 19.6057 15.1642 21.8306 14.1357 22C14.1357 22 13.9267 18.5826 14.0487 17.9969C14.1706 17.4113 13.7552 16.4688 13.7552 16.4688C14.7262 16.1055 16.2043 15.5847 16.7001 14.1874C17.0848 13.1032 17.3268 11.5288 16.2508 10.0489C16.2508 10.0489 16.5318 7.65809 15.9996 7.56548C15.4675 7.47287 13.8998 8.51192 13.8998 8.51192C13.4432 8.38248 12.4243 8.13476 12.0018 8.17939C11.5792 8.13476 10.5568 8.38248 10.1002 8.51192C10.1002 8.51192 8.53249 7.47287 8.00036 7.56548C7.46823 7.65809 7.74917 10.0489 7.74917 10.0489C6.67316 11.5288 6.91516 13.1032 7.2999 14.1874C7.79575 15.5847 9.27384 16.1055 10.2448 16.4688C10.2448 16.4688 9.82944 17.4113 9.95135 17.9969C10.0733 18.5826 9.86428 22 9.86428 22Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export const JavaScriptIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M11.7324 12V16C11.7324 17.1046 10.837 18 9.73244 18C8.99215 18 8.34581 17.5978 8 17"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+		/>
+		<path
+			d="M18.4668 13C18.121 12.4022 17.4747 12 16.7344 12H16.2344C15.4059 12 14.7344 12.6716 14.7344 13.5C14.7344 14.3284 15.4059 15 16.2344 15H17.2344C18.0628 15 18.7344 15.6716 18.7344 16.5C18.7344 17.3284 18.0628 18 17.2344 18H16.7344C15.9941 18 15.3477 17.5978 15.0019 17"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+		/>
+	</svg>
+);
+
+export const AwsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M2.5 12C2.5 7.52166 2.5 5.28249 3.89124 3.89124C5.28249 2.5 7.52166 2.5 12 2.5C16.4783 2.5 18.7175 2.5 20.1088 3.89124C21.5 5.28249 21.5 7.52166 21.5 12C21.5 16.4783 21.5 18.7175 20.1088 20.1088C18.7175 21.5 16.4783 21.5 12 21.5C7.52166 21.5 5.28249 21.5 3.89124 20.1088C2.5 18.7175 2.5 16.4783 2.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M18 12H16.2C15.5373 12 15 12.5373 15 13.2V13.8C15 14.4627 15.5373 15 16.2 15H16.8C17.4627 15 18 15.5373 18 16.2V16.8C18 17.4627 17.4627 18 16.8 18H15"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M8.5 12H10.5M12.5 12H10.5M10.5 12V18"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export const ReactIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M8 12C8 6.47715 9.79086 2 12 2C14.2091 2 16 6.47715 16 12C16 17.5228 14.2091 22 12 22C9.79086 22 8 17.5228 8 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M9.97531 8.61921C14.8173 5.85779 19.649 5.17014 20.7673 7.08331C21.8855 8.99648 18.8667 12.786 14.0247 15.5474C9.18271 18.3088 4.35098 18.9965 3.23277 17.0833C2.11455 15.1701 5.13329 11.3806 9.97531 8.61921Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M14.0247 8.61921C18.8667 11.3806 21.8855 15.1701 20.7673 17.0833C19.649 18.9965 14.8173 18.3088 9.97531 15.5474C5.13329 12.786 2.11455 8.99648 3.23277 7.08331C4.35098 5.17014 9.18271 5.85779 14.0247 8.61921Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M13.5 12C13.5 12.8284 12.8284 13.5 12 13.5C11.1716 13.5 10.5 12.8284 10.5 12C10.5 11.1716 11.1716 10.5 12 10.5C12.8284 10.5 13.5 11.1716 13.5 12Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+	</svg>
+);
+
+export const GitMergeIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M7 20C8.10457 20 9 19.1046 9 18C9 16.8954 8.10457 16 7 16C5.89543 16 5 16.8954 5 18C5 19.1046 5.89543 20 7 20Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M7 8C8.10457 8 9 7.10457 9 6C9 4.89543 8.10457 4 7 4C5.89543 4 5 4.89543 5 6C5 7.10457 5.89543 8 7 8Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M17 14C18.1046 14 19 13.1046 19 12C19 10.8954 18.1046 10 17 10C15.8954 10 15 10.8954 15 12C15 13.1046 15.8954 14 17 14Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path
+			d="M7.02165 8.27881V15.4068M14.4117 12.0043L10.0198 12.0043C8.92206 12.0043 6.86231 11.1249 7.01867 8.77952"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
+
+export const VueIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 48 48"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth={2}
+		strokeLinecap="round"
+		color={'#81c784'}
+		strokeLinejoin="round"
+		{...props}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-custom-icon">
+		<polygon fill="#81c784" points="23.987,17 18.734,8 2.974,8 23.987,44 45,8 29.24,8" />
+		<polygon fill="#455a64" points="29.24,8 23.987,17 18.734,8 11.146,8 23.987,30 36.828,8" />
+	</svg>
+);
+
+export const TailwindIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 24 24"
+		fill="none"
+		color={'#ffffff'}
+		stroke="currentColor"
+		strokeWidth={2}
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		className="icon icon-tabler icons-tabler-outline icon-tabler-brand-tailwind"
+		{...props}>
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M11.667 6c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 2 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968zm-4 6.5c-2.49 0 -4.044 1.222 -4.667 3.667c.933 -1.223 2.023 -1.68 3.267 -1.375c.71 .174 1.217 .68 1.778 1.24c.916 .912 1.975 1.968 4.288 1.968c2.49 0 4.044 -1.222 4.667 -3.667c-.933 1.223 -2.023 1.68 -3.267 1.375c-.71 -.174 -1.217 -.68 -1.778 -1.24c-.916 -.912 -1.975 -1.968 -4.288 -1.968z" />
+	</svg>
+);
+
+export const NextJsIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth={2}
+		strokeLinecap="round"
+		color={'#ffffff'}
+		strokeLinejoin="round"
+		{...props}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-brand-nextjs">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M9 15v-6l7.745 10.65a9 9 0 1 1 2.255 -1.993" />
+		<path d="M15 12v-3" />
+	</svg>
+);
+
+export const MongoDbIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 24 24"
+		fill="none"
+		stroke="currentColor"
+		color={'#ffffff'}
+		strokeWidth={2}
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		{...props}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-brand-mongodb">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M12 3v19" />
+		<path d="M18 11.227c0 3.273 -1.812 4.77 -6 9.273c-4.188 -4.503 -6 -6 -6 -9.273c0 -4.454 3.071 -6.927 6 -9.227c2.929 2.3 6 4.773 6 9.227z" />
+	</svg>
+);
+
+export const FlutterIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 48 48"
+		fill="none"
+		stroke="currentColor"
+		strokeWidth={2}
+		strokeLinecap="round"
+		color={'#40c4ff'}
+		strokeLinejoin="round"
+		{...props}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-custom-icon">
+		<polygon fill="#40c4ff" points="26,4 6,24 12,30 38,4" />
+		<polygon fill="#40c4ff" points="38,22 27,33 21,27 26,22" />
+		<rect
+			width="8.485"
+			height="8.485"
+			x="16.757"
+			y="28.757"
+			fill="#03a9f4"
+			transform="rotate(-45.001 21 33)"
+		/>
+		<polygon fill="#01579b" points="38,44 26,44 21,39 27,33" />
+		<polygon fill="#084994" points="21,39 30,36 27,33" />
+	</svg>
+);
+
+export const MySqlIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		width={48}
+		height={48}
+		viewBox="0 0 24 24"
+		color={'#ffffff'}
+		fill="none"
+		stroke="currentColor"
+		strokeWidth={2}
+		strokeLinecap="round"
+		strokeLinejoin="round"
+		{...props}
+		className="icon icon-tabler icons-tabler-outline icon-tabler-brand-mysql">
+		<path stroke="none" d="M0 0h24v24H0z" fill="none" />
+		<path d="M13 21c-1.427 -1.026 -3.59 -3.854 -4 -6c-.486 .77 -1.501 2 -2 2c-1.499 -.888 -.574 -3.973 0 -6c-1.596 -1.433 -2.468 -2.458 -2.5 -4c-3.35 -3.44 -.444 -5.27 2.5 -3h1c8.482 .5 6.421 8.07 9 11.5c2.295 .522 3.665 2.254 5 3.5c-2.086 -.2 -2.784 -.344 -3.5 0c.478 1.64 2.123 2.2 3.5 3" />
+		<path d="M9 7h.01" />
+	</svg>
+);
+
+export const ProgrammingFlagIcon = (props: React.SVGProps<SVGSVGElement>) => (
+	<svg
+		xmlns="http://www.w3.org/2000/svg"
+		viewBox="0 0 24 24"
+		width={48}
+		height={48}
+		color={'#ffffff'}
+		fill={'none'}
+		{...props}>
+		<path
+			d="M4 7C4 5.11438 4 4.17157 4.58579 3.58579C5.17157 3 6.11438 3 8 3H15C17.8284 3 19.2426 3 20.1213 3.87868C21 4.75736 21 6.17157 21 9C21 11.8284 21 13.2426 20.1213 14.1213C19.2426 15 17.8284 15 15 15H4V7Z"
+			stroke="currentColor"
+			strokeWidth="1.5"
+		/>
+		<path d="M4 21L4 8" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" />
+		<path
+			d="M16 7L17.2265 8.05719C17.7422 8.50163 18 8.72386 18 9C18 9.27614 17.7422 9.49836 17.2265 9.94281L16 11"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M9 7L7.77346 8.05719C7.25782 8.50163 7 8.72386 7 9C7 9.27614 7.25782 9.49836 7.77346 9.94281L9 11"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+		<path
+			d="M13.5 7L11.5 11"
+			stroke="currentColor"
+			strokeWidth="1.5"
+			strokeLinecap="round"
+			strokeLinejoin="round"
+		/>
+	</svg>
+);
